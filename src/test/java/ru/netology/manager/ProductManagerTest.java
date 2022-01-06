e9893aada53ea6bf1fb680f6ae6c74d7e5114bcd
Smetskii Ivan <smetskii.ivan@gmail.com>
2022-01-07 02:27:28 +0300

    Some corrections

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    void searchBy() {
+    }
+}
